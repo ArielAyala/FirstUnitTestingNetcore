d0bd741147dc099f8f758d5601750b3cebc8e831
Ariel Ayala <arielayalamail@gmail.com>
2022-03-12 16:30:59 -0300

    Test with list and exceptions

---
+++ FirstUnitTestingNetcore/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstUnitTestingNetcore
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class CollectionsSample
+    {
+        public static Customer GetCustomer(Customer[] customers, int index)
+        {
+            return customers[index];
+        }
+    }
+
+
+}
---
+++ FirstUnitTestingNetcoreTest/CollectionSampleTest.cs
@@ -0,0 +1,45 @@
+﻿using FirstUnitTestingNetcore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstUnitTestingNetcoreTest
+{
+    [TestClass]
+    public class CollectionSampleTest
+    {
+        [TestMethod]
+        public void GetCustomer_ShouldGetSameCustomer()
+        {
+            //Arrange
+            Customer[] customers = new Customer[2] { new Customer { FirstName = "Ariel", LastName = "Ayala" }, new Customer { FirstName = "Felix", LastName = "Ayala" } };
+
+            int index = 1;
+            Customer expect = customers[1];
+
+            //Act
+            Customer actual = CollectionsSample.GetCustomer(customers, index);
+
+            //Assert
+            Assert.IsInstanceOfType(actual, typeof(Customer));
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void GetCustomer_ShouldThrowIndexOutofRangeException()
+        {
+            //Arrange
+            Customer[] customers = new Customer[2] { new Customer { FirstName = "Ariel", LastName = "Ayala" }, new Customer { FirstName = "Felix", LastName = "Ayala" } };
+
+            int index = 3;
+
+            //Act
+            CollectionsSample.GetCustomer(customers, index);
+
+            //Assert
+
+        }
+    }
+}
